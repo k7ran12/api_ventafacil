093485b05e07f92bf9b0f02992e14d4e00051fce
Cristian Antonio <tauro24_vs@hotmail.com>
2022-06-04 02:18:59 +0200

    Crear venta y ver ventas

--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Venta extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'numero_venta',
+        'fecha_venta',
+        'tipo_pago',
+        'numero_comprobante',
+        'tipo_comprobante',
+        'sumas',
+        'iva',
+        'exento',
+        'retenido',
+        'descuento',
+        'total',
+        'sonlestras',
+        'pago_efectivo',
+        'pago_tarjeta',
+        'numero_tarjeta',
+        'tarjeta_habiente',
+        'cambio',
+        'estado',
+        'user_id',
+        'cliente_id',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function cliente(){
+        return $this->belongsTo(Cliente::class);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login','App\Http\Controllers\UserController@login');
 Route::post('register','App\Http\Controllers\UserController@register');
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('logout','App\Http\Controllers\UserController@logout');    
+    Route::post('crear-venta', 'App\Http\Controllers\VentaController@crearVenta');
+    Route::get('ventas', 'App\Http\Controllers\VentaController@ventas');
+});
